60446b7245438dcf2f96647c49672313074b3490
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2017-06-21 11:15:00 +0200

    ajout du lock sur les niveau

--- src/browser/dungeons/Dungeon.js
+++ src/browser/dungeons/Dungeon.js
@@ -22,22 +22,32 @@ const Dungeon = ({ dungeon,loadWorldMap,viewer }: Props) => {
             cursor: 'pointer',
         },
     };
-
-    return (
+    if(dungeon.lock === true){
+      return (
         <div className="one-level">
-            <div className="level-lock"></div>
-            <div className="choose-level" onClick={() => loadWorldMap(dungeon,viewer)}>
-              <span>1</span>
-            </div>
-            <Text style={styles.title}
-                  onClick={() => loadWorldMap(dungeon,viewer)}>
-                Description : {dungeon.description}
-            </Text>
-            {/*<WorldMap*/}
-                {/*worldmap={dungeon.worldmap}*/}
-            {/*></WorldMap>*/}
+          <div className="level-lock"></div>
+          <Text style={styles.title}
+                onClick={() => loadWorldMap(dungeon,viewer)}>
+            Description : {dungeon.description}
+          </Text>
         </div>
-    );
+      );
+    } else {
+      return (
+        <div className="one-level">
+          <div className="choose-level" onClick={() => loadWorldMap(dungeon,viewer)}>
+            <span>1</span>
+          </div>
+          <Text style={styles.title}
+                onClick={() => loadWorldMap(dungeon,viewer)}>
+            Description : {dungeon.description}
+          </Text>
+          {/*<WorldMap*/}
+          {/*worldmap={dungeon.worldmap}*/}
+          {/*></WorldMap>*/}
+        </div>
+      );
+    }
 };
 
 Dungeon.propTypes = {
